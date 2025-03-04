79fc276bd8ea0068b5ee8002c5d56894ff16d4ec
Brittany Halterman <halterma@gmail.com>
2025-03-04 14:27:50 -0500

    Practice JavaScript array method: filter()

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('Hello Node');
+const numbers = [10, 20, 30, 40, 50];
+
+const higherThan22 = numbers.filter((num) => num > 22);
+console.log(higherThan22);
+
+const students = [
+  {
+    name: 'Kelly',
+    age: 27,
+    eyeColor: 'blue',
+  },
+  {
+    name: 'George',
+    age: 19,
+    eyeColor: 'brown',
+  },
+  {
+    name: 'Beth',
+    age: 43,
+    eyeColor: 'brown',
+  },
+];
+
+const brownEyedStudents = students.filter(
+  (student) => student.eyeColor === 'brown'
+);
+console.log(brownEyedStudents);
+
+const under30 = students.filter((student) => student.age < 30);
+console.log(under30);
